c9d6dc228547bd951b39b97126ab316136a54f4e
Mingming Bai <mingmingbai78@gmail.com>
2020-01-21 11:28:03 -0700

    add fetch api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <div v-for="game in games" :key="game.id">
+      {{ game.id }} - {{ game.name }}
+    </div>
     <GameList />
   </div>
 </template>
@@ -12,6 +15,24 @@ export default {
   name: "home",
   components: {
     GameList
+  },
+  data() {
+    return {
+      games: []
+    };
+  },
+  methods: {},
+  mounted: function() {
+    console.log("home mounted");
+    fetch("http://api.planets.nu/games/list", {
+      method: "get"
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        this.games = jsonData;
+      });
   }
 };
 </script>
