f0fb4e1d89c3a16bef6ef1501da845b09c0be3e2
Mingming Bai <mingmingbai78@gmail.com>
2020-01-21 12:15:35 -0700

    date format

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,36 +9,38 @@
         <th>Host Days</th>
         <th>Password</th>
       </tr>
-      <tr v-for="game in joiningGame" :key="game.id">
+      <tr v-for="game in joiningGames" :key="game.id">
         <td>{{ game.name }}</td>
         <td>{{ game.shortdescription }}</td>
-        <td>{{ game.datecreated }}</td>
-        <td>{{ game.createdby }}</td>
+        <td>{{ game.datecreated | dateFormat }}</td>
+        <td>{{ game.createdby === "none" ? "" : game.createdby }}</td>
         <td>{{ game.hostdays }}</td>
         <td>{{ game.haspassword ? "ps" : "" }}</td>
       </tr>
     </table>
-
-    <GameList />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import GameList from "@/components/GameList.vue";
+import moment from "moment";
 
 export default {
   name: "home",
-  components: {
-    GameList
-  },
+
   data() {
     return {
       games: []
     };
   },
+  filters: {
+    dateFormat: function(value) {
+      let date = moment(value).format("MMMM Do, YYYY");
+      return date;
+    }
+  },
   computed: {
-    joiningGame() {
+    joiningGames() {
       return this.games.filter(game => game.statusname == "Joining");
     }
   },
